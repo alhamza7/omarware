3954eb2694928dff32c3f54239977ac8c93d60da
alhamza7 <88052469+alhamza7@users.noreply.github.com>
2026-01-15 11:37:44 +0300

    11

--- addons/pos_perfume_custom/models/__init__.py
+++ addons/pos_perfume_custom/models/__init__.py
@@ -4,10 +4,7 @@ from . import product_extended
 from . import pos_perfume_order
 from . import pos_perfume_order_line
 from . import pos_whatsapp_wizard
-from . import pos_perfume_session
-from . import product_template
-from . import product_product
-from . import product_packaging
 from . import res_config_settings
+
 
 
